b619f67d21837b644c29ddeeedd9e9875aa5d10d
KhaledBahjat <khaledbahjat0@gmail.com>
2025-08-01 00:04:46 +0300

    upload  file ui done

--- flutter_logic/lib/screens/attachment&images_screens/upload_file.dart
+++ flutter_logic/lib/screens/attachment&images_screens/upload_file.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 class UploadFile extends StatelessWidget {
   const UploadFile({super.key});
@@ -18,7 +20,110 @@ class UploadFile extends StatelessWidget {
               height: 55,
               decoration: BoxDecoration(
                 color: Colors.white60,
-                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Row(
+                children: [
+                  SvgPicture.asset(
+                    'assets/svgs/doc.svg',
+                    width: 55,
+                  ),
+
+                  SizedBox(
+                    width: 8,
+                  ),
+
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Upload File',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        'pdf, docx, doc, jpg',
+                      ),
+                    ],
+                  ),
+
+                  Spacer(),
+
+                  PopupMenuButton(
+                    itemBuilder: (context) {
+                      return [
+                        // Upload
+                        PopupMenuItem(
+                          child: Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.arrow_up_doc,
+                              ),
+                              SizedBox(
+                                width: 7,
+                              ),
+                              Text(
+                                'Upload',
+                              ),
+                            ],
+                          ),
+                        ),
+
+                        // Veiw
+                        PopupMenuItem(
+                          child: Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.eye,
+                              ),
+                              SizedBox(
+                                width: 7,
+                              ),
+                              Text(
+                                'View',
+                              ),
+                            ],
+                          ),
+                        ),
+                        // Change
+                        PopupMenuItem(
+                          child: Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.refresh,
+                              ),
+                              SizedBox(
+                                width: 7,
+                              ),
+                              Text(
+                                'Change',
+                              ),
+                            ],
+                          ),
+                        ),
+                        // delete
+                        PopupMenuItem(
+                          child: Row(
+                            children: [
+                              Icon(
+                                CupertinoIcons.delete,
+                                color: Colors.red,
+                              ),
+                              SizedBox(
+                                width: 7,
+                              ),
+                              Text(
+                                'Delete',
+                                style: TextStyle(color: Colors.red),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ];
+                    },
+                  ),
+                ],
               ),
             ),
           ],
